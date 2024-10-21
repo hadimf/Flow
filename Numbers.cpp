c9a6e04aed1e495594548f11af728cf0e734aca9
Hadiii <h.mousavifakhr@gmail.com>
2024-10-21 10:57:52 +0330

    Numbers Operator

---
+++ Numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Arithmetic operations :"<<endl;
+    int num1,num2;
+    cout<<"Enter Number1:"<<endl;
+    cin>>num1;
+    cout<<"Enter Number2:"<<endl;
+    cin>>num2;
+
+//    +
+//cout<<num1+num2;
+
+// -
+//cout<<num1-num2;
+
+//%
+cout<<num1%num2;
+
+
+
+
+}
