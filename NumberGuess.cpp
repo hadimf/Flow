8ce9b29cace56c35805404577a4c3a1dc07e00f6
Hadiii <h.mousavifakhr@gmail.com>
2024-10-22 09:48:31 +0330

    get points

---
+++ NumberGuess.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+void showGame(string inputs[4][4]){
+    cout<<"    0 1 2 3"<<endl<<endl;
+    cout<<"0   "<<inputs[0][0]<<" "<<inputs[0][1]<<" "<<inputs[0][2]<<" "<<inputs[0][3]<<endl;
+    cout<<"1   "<<inputs[1][0]<<" "<<inputs[1][1]<<" "<<inputs[1][2]<<" "<<inputs[1][3]<<endl;
+    cout<<"2   "<<inputs[2][0]<<" "<<inputs[2][1]<<" "<<inputs[2][2]<<" "<<inputs[2][3]<<endl;
+    cout<<"3   "<<inputs[3][0]<<" "<<inputs[3][1]<<" "<<inputs[3][2]<<" "<<inputs[3][3]<<endl;
+
+    cout<<"------------------------------------------------------"<<endl;
+
+}
+
+
+int main() {
+    cout<<"Number Guess:"<<endl;
+    string input[4][4]= {{"1","1","2","2"},
+                      {"3","3","4","4"},
+                      {"5","5","6","6"},
+                      {"7","7","8","8"}};
+
+    string emptyInput[4][4] ={{" "," ", " ", " "},
+                              {" "," ", " "," "},
+                              {" ", " ", " ", " "},
+                              {" ", " ", " ", " "}};
+
+
+    showGame(input);
+    showGame(emptyInput);
+
+
+    int point1x, point1y, point2x, point2y;
+//    Get Points 
+    cout<<"Enter Point1: "<<endl;
+    cin>>point1x>>point1y;
+    cout<<"Enter Point2: "<<endl;
+    cin>>point2x>>point2y;
+
+
+
+    return 0;
+}
